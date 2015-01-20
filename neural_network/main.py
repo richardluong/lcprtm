aaef36004ede2fd937db47d54c41a2fef93c0274
kerryz <kerryzhang92@gmail.com>
2015-01-20 20:12:43 +0800

    Run main to see xBleu change after each gradient descent

--- neural_network/main.py
+++ neural_network/main.py
@@ -15,10 +15,14 @@ Goal: to learn W1 and W2
 from __future__ import division
 
 import numpy as np
+import random
 from cptm_neural_network import CPTMNeuralNetwork, get_error_term_dict
 from get_lists_and_dictionaries import get_everything
 from xbleu import xbleu, get_Ej_translation_probability_list
 from gensim import corpora
+
+
+debug_mode = True
 
 
 def main():
@@ -27,10 +31,18 @@ def main():
     nn = CPTMNeuralNetwork([W1.shape[0], 100, 100], [W1, W2])
     dictionary = corpora.Dictionary.load("../lda/data/dictionary.dict")
 
-    for i in xrange(1):
+    training_set_size = 887  # TODO: how to check how many?
+    training_order_list = range(training_set_size)
+    random.shuffle(training_order_list)
+    # TODO: while not converged: how to check convergence? Early stop method
+
+    xBleu_history = []
+    xBleu_change_history = []
+    for i in training_order_list:
         (phrase_pair_dict_all, phrase_pair_dict_n_list,
-            total_base_score_list, sbleu_score_list) = get_everything(0)
-        xblue_i = xbleu(nn, total_base_score_list, sbleu_score_list, phrase_pair_dict_n_list, dictionary)
+            total_base_score_list, sbleu_score_list) = get_everything(i)
+        xblue_i = xbleu(nn, total_base_score_list, sbleu_score_list,
+                        phrase_pair_dict_n_list, dictionary)
         Ej_translation_probability_list = get_Ej_translation_probability_list(
             nn, total_base_score_list, phrase_pair_dict_n_list, dictionary)
 
@@ -39,11 +51,53 @@ def main():
             sbleu_score_list, xblue_i,
             Ej_translation_probability_list)
 
-        print "Weights before update"
-        print nn.weights
-        nn.update_mini_batch(phrase_pair_dict_all, 1000000, dictionary, error_term_dict_i)
-        print "Weights after update"
-        print nn.weights
+        if False:
+            print "Weights before update"
+            print
+            print "W1"
+            print nn.weights[0]
+            print
+            print "W2"
+            print nn.weights[1]
+            print
+            sum_W1_before = sum(sum(nn.weights[0]))
+            print "W1 sum BEFORE:", sum_W1_before
+            sum_W2_before = sum(sum(nn.weights[1]))
+            print "W2 sum BEFORE:", sum_W2_before
+
+        nn.update_mini_batch(phrase_pair_dict_all, 0.001, dictionary, error_term_dict_i)
+
+        if False:
+            print "Weights after update"
+            print
+            print "W1"
+            print nn.weights[0]
+            print
+            print "W2"
+            print nn.weights[1]
+            print
+            sum_W1_after = sum(sum(nn.weights[0]))
+            print "W1 sum AFTER:", sum_W1_after
+            sum_W2_after = sum(sum(nn.weights[1]))
+            print "W2 sum AFTER:", sum_W2_after
+
+            print "W1 sum difference:", sum_W1_after - sum_W1_before
+            print "W2 sum difference:", sum_W2_after - sum_W2_before
+
+        # xBleu increases?
+        if debug_mode:
+            xblue_i_after = xbleu(nn, total_base_score_list, sbleu_score_list,
+                                  phrase_pair_dict_n_list, dictionary)
+
+            xBleu_history.append((xblue_i, xblue_i_after))
+            xBleu_change_history.append(xblue_i_after - xblue_i)
+            print "-------------------------------------------------------------"
+            print "xBleu history: [(xBleu_before_gradient_descent, xBleu_after_gradient_descent)]"
+            print xBleu_history
+            print
+            print "xBleu_change_history [xBleu_after - xBleu_before]"
+            print xBleu_change_history
+            print "-------------------------------------------------------------"
 
 
 class LazyFileReader(object):
@@ -58,12 +112,12 @@ class LazyFileReader(object):
 
 def get_W1_from_text_file(path):
     with open(path, 'r') as W1_text_file:
-        line = map(lambda x: float(x), (W1_text_file.readline().strip() + " 0.0").split(' '))
-        W1 = np.zeros((100, len(line)))  # TODO: FIX W1_TEXT ONE LESS ENTRY
+        line = map(lambda x: float(x), (W1_text_file.readline().strip()).split(' '))
+        W1 = np.zeros((100, len(line)))
         W1[0] = line
 
         for i, line in enumerate(W1_text_file):
-            line = map(lambda x: float(x), (line.strip() + " 0.0").split(' '))
+            line = map(lambda x: float(x), (line.strip()).split(' '))
             W1[i+1] = line
     return W1.transpose()
 
