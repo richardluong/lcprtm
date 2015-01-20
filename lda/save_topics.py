cfc3ee746d0470f82322609af9167c5d5c9e3222
Richard Luong <richardluong92@gmail.com>
2015-01-20 14:24:47 +0800

    Modified w1 init

--- lda/save_topics.py
+++ lda/save_topics.py
@@ -3,11 +3,12 @@ import logging, gensim, bz2
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 lda = models.LdaModel.load('data/model.lda')
+dictionary = corpora.Dictionary.load('data/dictionary.dict')
 
 f = open("data/weight_initialization.txt", "w+")
 i = 0
 
-topics = lda.show_topics(formatted = False, num_topics=-1, num_words=-1)
+topics = lda.show_topics(formatted = False, num_topics=lda.num_topics, num_words=len(dictionary))
 for topic in topics:
 	topic.sort(key=lambda tup: int(tup[1]))
 	string = ""
