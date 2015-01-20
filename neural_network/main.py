8936c374c5402f72e3617e356ed16403c553b8fd
kerryz <kerryzhang92@gmail.com>
2015-01-20 23:12:35 +0800

    Added momentum term and convergence check through test set

--- neural_network/main.py
+++ neural_network/main.py
@@ -26,6 +26,7 @@ debug_mode = True
 
 
 def main():
+    print "Loading and initializing system"
     W1 = get_W1_from_text_file("../lda/data/weight_initialization.txt")
     W2 = np.identity(100)
     nn = CPTMNeuralNetwork([W1.shape[0], 100, 100], [W1, W2])
@@ -39,74 +40,113 @@ def main():
             training_set_size += 1
     training_set_size -= 1  # ends with empty line
 
+    # randomize training samples
+    # TODO: REMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOVE
+    training_set_size = 5
     training_order_list = range(training_set_size)
-    random.shuffle(training_order_list)
-    # TODO: while not converged: how to check convergence? Early stop method
+    # TODO: SHUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUFLE
+    #random.shuffle(training_order_list)
 
+    # separate into test and training set
+    test_size = max(1, int(0.01*training_set_size))
+    test_order_list = training_order_list[:test_size]
+    training_order_list = training_order_list[test_size:]
+
+    # initialize variables
+    d_theta_old = [0, 0]  # momentum terms
+
+    old_loss_value_test_set = get_average_loss_value_of_test_sample(test_order_list, nn, dictionary)
+    converged = False
+    epoch_count = 0
+
+    # For debug
     xBleu_history = []
     xBleu_change_history = []
-    d_theta_old = [0, 0]  # momentum terms
-    for i in training_order_list:
-        (phrase_pair_dict_all, phrase_pair_dict_n_list,
-            total_base_score_list, sbleu_score_list) = get_everything(i)
-        xblue_i = xbleu(nn, total_base_score_list, sbleu_score_list,
-                        phrase_pair_dict_n_list, dictionary)
-        Ej_translation_probability_list = get_Ej_translation_probability_list(
-            nn, total_base_score_list, phrase_pair_dict_n_list, dictionary)
 
-        error_term_dict_i = get_error_term_dict(
-            phrase_pair_dict_all, phrase_pair_dict_n_list,
-            sbleu_score_list, xblue_i,
-            Ej_translation_probability_list)
+    # train until overfit (early stop)
+    print "Start training"
+    while not converged:
+        theta_previous = nn.weights
+        for i in training_order_list:
+            (phrase_pair_dict_all, phrase_pair_dict_n_list,
+                total_base_score_list, sbleu_score_list) = get_everything(i)
+            xblue_i = xbleu(nn, total_base_score_list, sbleu_score_list,
+                            phrase_pair_dict_n_list, dictionary)
+            Ej_translation_probability_list = get_Ej_translation_probability_list(
+                nn, total_base_score_list, phrase_pair_dict_n_list, dictionary)
 
-        if False:
-            print "Weights before update"
-            print
-            print "W1"
-            print nn.weights[0]
-            print
-            print "W2"
-            print nn.weights[1]
-            print
-            sum_W1_before = sum(sum(nn.weights[0]))
-            print "W1 sum BEFORE:", sum_W1_before
-            sum_W2_before = sum(sum(nn.weights[1]))
-            print "W2 sum BEFORE:", sum_W2_before
+            error_term_dict_i = get_error_term_dict(
+                phrase_pair_dict_all, phrase_pair_dict_n_list,
+                sbleu_score_list, xblue_i,
+                Ej_translation_probability_list)
 
-        d_theta_old = nn.update_mini_batch(
-            phrase_pair_dict_all, 0.001, dictionary, error_term_dict_i, d_theta_old)
+            if False:
+                print "Weights before update"
+                print
+                print "W1"
+                print nn.weights[0]
+                print
+                print "W2"
+                print nn.weights[1]
+                print
+                sum_W1_before = sum(sum(nn.weights[0]))
+                print "W1 sum BEFORE:", sum_W1_before
+                sum_W2_before = sum(sum(nn.weights[1]))
+                print "W2 sum BEFORE:", sum_W2_before
 
-        if False:
-            print "Weights after update"
-            print
-            print "W1"
-            print nn.weights[0]
-            print
-            print "W2"
-            print nn.weights[1]
-            print
-            sum_W1_after = sum(sum(nn.weights[0]))
-            print "W1 sum AFTER:", sum_W1_after
-            sum_W2_after = sum(sum(nn.weights[1]))
-            print "W2 sum AFTER:", sum_W2_after
+            d_theta_old = nn.update_mini_batch(
+                phrase_pair_dict_all, 0.001, dictionary, error_term_dict_i, d_theta_old)
 
-            print "W1 sum difference:", sum_W1_after - sum_W1_before
-            print "W2 sum difference:", sum_W2_after - sum_W2_before
+            if False:
+                print "Weights after update"
+                print
+                print "W1"
+                print nn.weights[0]
+                print
+                print "W2"
+                print nn.weights[1]
+                print
+                sum_W1_after = sum(sum(nn.weights[0]))
+                print "W1 sum AFTER:", sum_W1_after
+                sum_W2_after = sum(sum(nn.weights[1]))
+                print "W2 sum AFTER:", sum_W2_after
 
-        # xBleu increases?
-        if False:
-            xblue_i_after = xbleu(nn, total_base_score_list, sbleu_score_list,
-                                  phrase_pair_dict_n_list, dictionary)
+                print "W1 sum difference:", sum_W1_after - sum_W1_before
+                print "W2 sum difference:", sum_W2_after - sum_W2_before
 
-            xBleu_history.append((xblue_i, xblue_i_after))
-            xBleu_change_history.append(xblue_i_after - xblue_i)
-            print "-------------------------------------------------------------"
-            print "xBleu history: [(xBleu_before_gradient_descent, xBleu_after_gradient_descent)]"
-            print xBleu_history
-            print
-            print "xBleu_change_history [xBleu_after - xBleu_before]"
-            print xBleu_change_history
-            print "-------------------------------------------------------------"
+            # xBleu increases?
+            if False:
+                xblue_i_after = xbleu(nn, total_base_score_list, sbleu_score_list,
+                                      phrase_pair_dict_n_list, dictionary)
+
+                xBleu_history.append((xblue_i, xblue_i_after))
+                xBleu_change_history.append(xblue_i_after - xblue_i)
+                print "-------------------------------------------------------------"
+                print "xBleu history: [(xBleu_before_gradient_descent, xBleu_after_gradient_descent)]"
+                print xBleu_history
+                print
+                print "xBleu_change_history [xBleu_after - xBleu_before]"
+                print xBleu_change_history
+                print "-------------------------------------------------------------"
+
+        epoch_count += 1
+        print "Finished epoch number:", epoch_count
+
+        # calculate loss function on test set after each epoch using updated weights
+        loss_value_test_set = get_average_loss_value_of_test_sample(test_order_list, nn, dictionary)
+        print "Old average loss function value (-xBleu) over test set:"
+        print old_loss_value_test_set
+        print "Average loss function value with updated weights:"
+        print loss_value_test_set
+        print "Difference (new - old):"
+        print loss_value_test_set - old_loss_value_test_set
+        if loss_value_test_set > old_loss_value_test_set:
+            converged = True
+            print "CONVERGED!!!!!!!!!!!!"
+            print "Saving weights from previous epoch to file"
+            # TODO: Save theta_previous
+        else:
+            print "Not converged, keep training..."
 
 
 class LazyFileReader(object):
@@ -129,6 +169,17 @@ def get_W1_from_text_file(path):
             line = map(lambda x: float(x), (line.strip()).split(' '))
             W1[i+1] = line
     return W1.transpose()
+
+
+def get_average_loss_value_of_test_sample(test_order_list, nn, dictionary):
+    loss_value_test_set = 0
+    for t_i in test_order_list:
+        (phrase_pair_dict_n_listase_pair_dict_all, phrase_pair_dict_n_list,
+            total_base_score_list, sbleu_score_list) = get_everything(t_i)
+        xBlue_t_i = xbleu(nn, total_base_score_list, sbleu_score_list,
+                          phrase_pair_dict_n_list, dictionary)
+        loss_value_test_set -= xBlue_t_i
+    return loss_value_test_set/len(test_order_list)
 
 
 if __name__ == "__main__":
